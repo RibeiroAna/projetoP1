d1b752d5f73a5e4be5de051251e487e549705e4b
Matteus Silva <matteus.silva@ccc.ufcg.edu.br>
2015-02-10 19:09:47 -0300

    corrigindo bugs de identacao e add pausa(por enquanto vai pro menu).

--- level1.py
+++ level1.py
@@ -2,9 +2,9 @@ import pygame, sys, time, personagem
 
 
 class Nivel1():
-	bg_num = 1
-	bg1 = "img/sayonara.png"
-	
+    bg_num = 1
+    bg1 = "img/sayonara.png"
+    
 
 def jogar():
     window = pygame.display.set_mode((800, 600)) 
@@ -16,27 +16,30 @@ def jogar():
     pygame.key.set_repeat(1, 200)
     window.blit(player.walking_frames_r[0], (player.pos_x, player.pos_y))
     window.blit(fundo, (0, 0))
-
+    # botao que define a pausa (por enquanto voltando para o menu)
+    pausa = False
     while True:
     
         window.blit(player.walking_frames_r[0], (player.pos_x, player.pos_y))
-  
+        if pausa == True:
+            break
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-                 
             elif event.type == pygame.KEYDOWN:
-		      	if event.key == pygame.K_LEFT:
-			      	if (player.pos_x != 0):
-				      	player.esquerda()
-                        if event.key == pygame.K_RIGHT:
-				     if (player.pos_x != 800):
-					    player.direita()
-				     else:
-					    player.pos_x = 0
-                        if event.key == pygame.K_UP:
-				     player.cima()
-					
+                if event.key == pygame.K_ESCAPE:
+                    pausa = True
+                if event.key == pygame.K_LEFT:
+                    if (player.pos_x != 0):
+                        player.esquerda()
+                if event.key == pygame.K_RIGHT:
+                    if (player.pos_x != 800):
+                        player.direita()
+                    else:
+                        player.pos_x = 0
+                if event.key == pygame.K_UP:
+                    player.cima()
+                    
     
         pygame.display.update()
 
